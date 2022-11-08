20912278fc39689094b28a5784d148c840a05b21
Just gemer <115401329+Just-gemer@users.noreply.github.com>
2022-11-08 23:24:24 +0500

    Update nxvideoconv.bat

--- nxvideoconv.bat
+++ nxvideoconv.bat
@@ -1,8 +1,10 @@
 @echo off
 title NX Webm Tool By Just gemer
 echo PLEASE DONT CLOSE WINDOW WHILE CONVERTING
-set /P mapname=Enter mapname: 
-ffmpeg.exe -i input.webm -threads:v 1 -metadata title='1216x720' -sws_flags bicubic -codec:v libvpx -r:v 25  -b:v 9900k -bufsize 9900k -g 120 -rc_lookahead 16 -profile:v 1 -qmax 51 -qmin 11 -slices 4 -quality realtime -an -vol 0 -b:v 9900k  -aspect 16:9 -b:v 9900k -filter:v scale=1980:1080 %mapname%.vp9.720.webm
+set /P mapname=Enter mapname:
+set /P videotitle=Enter video title:
+set /P videoex=Enter video extension type:
+ffmpeg.exe -i %videotitle%.%videoex% -threads:v 3 -sws_flags bicubic -c:v libvpx -r:v 25 -b:v 8000k -bufsize 8000k -g 120 -rc_lookahead 16 -profile:v 0 -qmax 51 -qmin 11 -slices 4 -quality realtime -an -aspect 16:9 -filter:v scale=1216:720 %mapname%.vp9.720.webm
 mkclean.exe %mapname%.vp9.720.webm
 del %mapname%.vp9.720.webm
 ren clean.%mapname%.vp9.720.webm %mapname%.vp9.720.webm
